63b6b5f93df9e9506405b53cc1eef2299bc5c0cb
Inthira4548 <kineqwasg@gmail.com>
2025-12-17 11:30:35 +0700

    Add project files.

---
+++ Form2.Designer.cs
@@ -0,0 +1,450 @@
+﻿namespace Winform.userDefineFunction
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            groupBox1 = new GroupBox();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            groupBox2 = new GroupBox();
+            label15 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            button2 = new Button();
+            label13 = new Label();
+            textBox4 = new TextBox();
+            label14 = new Label();
+            groupBox3 = new GroupBox();
+            label20 = new Label();
+            label19 = new Label();
+            label18 = new Label();
+            button3 = new Button();
+            textBox3 = new TextBox();
+            label17 = new Label();
+            label16 = new Label();
+            textBox5 = new TextBox();
+            label21 = new Label();
+            button4 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(40, 67);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 20);
+            label1.TabIndex = 0;
+            label1.Text = "ความสูง";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(40, 128);
+            label2.Name = "label2";
+            label2.Size = new Size(85, 20);
+            label2.TabIndex = 1;
+            label2.Text = "ความยาวฐาน";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(143, 128);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(143, 67);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = SystemColors.Info;
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Location = new Point(296, 32);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(422, 291);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "คำนวณพื้นที่สามเหลี่ยม";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(295, 224);
+            label7.Name = "label7";
+            label7.Size = new Size(47, 20);
+            label7.TabIndex = 9;
+            label7.Text = "cm^2";
+            // 
+            // label6
+            // 
+            label6.BackColor = SystemColors.ControlLightLight;
+            label6.Location = new Point(165, 195);
+            label6.Name = "label6";
+            label6.Size = new Size(115, 59);
+            label6.TabIndex = 8;
+            label6.Text = "label6";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(56, 190);
+            label5.Name = "label5";
+            label5.Size = new Size(97, 20);
+            label5.TabIndex = 7;
+            label5.Text = "พืนที่สามเหลี่ยม";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(274, 135);
+            label4.Name = "label4";
+            label4.Size = new Size(29, 20);
+            label4.TabIndex = 6;
+            label4.Text = "cm";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(274, 74);
+            label3.Name = "label3";
+            label3.Size = new Size(29, 20);
+            label3.TabIndex = 5;
+            label3.Text = "cm";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(309, 93);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 55);
+            button1.TabIndex = 4;
+            button1.Text = "go";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = SystemColors.Info;
+            groupBox2.Controls.Add(label15);
+            groupBox2.Controls.Add(label8);
+            groupBox2.Controls.Add(label9);
+            groupBox2.Controls.Add(label10);
+            groupBox2.Controls.Add(label11);
+            groupBox2.Controls.Add(label12);
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(label13);
+            groupBox2.Controls.Add(textBox4);
+            groupBox2.Controls.Add(label14);
+            groupBox2.Location = new Point(4, 393);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(417, 275);
+            groupBox2.TabIndex = 5;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "คำนวณพื้นที่วงกลม";
+            // 
+            // label15
+            // 
+            label15.BackColor = SystemColors.ControlLightLight;
+            label15.Location = new Point(148, 128);
+            label15.Name = "label15";
+            label15.Size = new Size(87, 35);
+            label15.TabIndex = 10;
+            label15.Text = "label15";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(278, 222);
+            label8.Name = "label8";
+            label8.Size = new Size(47, 20);
+            label8.TabIndex = 9;
+            label8.Text = "cm^2";
+            // 
+            // label9
+            // 
+            label9.BackColor = SystemColors.ControlLightLight;
+            label9.Location = new Point(148, 194);
+            label9.Name = "label9";
+            label9.Size = new Size(115, 59);
+            label9.TabIndex = 8;
+            label9.Text = "label9";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(49, 213);
+            label10.Name = "label10";
+            label10.Size = new Size(75, 20);
+            label10.TabIndex = 7;
+            label10.Text = "พืนที่วงกลม";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(251, 152);
+            label11.Name = "label11";
+            label11.Size = new Size(29, 20);
+            label11.TabIndex = 6;
+            label11.Text = "cm";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(257, 60);
+            label12.Name = "label12";
+            label12.Size = new Size(29, 20);
+            label12.TabIndex = 5;
+            label12.Text = "cm";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(339, 39);
+            button2.Name = "button2";
+            button2.Size = new Size(66, 48);
+            button2.TabIndex = 4;
+            button2.Text = "go";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(54, 53);
+            label13.Name = "label13";
+            label13.Size = new Size(70, 20);
+            label13.TabIndex = 0;
+            label13.Text = "รัศมีวงกลม";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(126, 53);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 3;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(40, 128);
+            label14.Name = "label14";
+            label14.Size = new Size(66, 20);
+            label14.TabIndex = 1;
+            label14.Text = "เส้นรอบวง";
+            // 
+            // groupBox3
+            // 
+            groupBox3.BackColor = SystemColors.ActiveCaption;
+            groupBox3.Controls.Add(label20);
+            groupBox3.Controls.Add(label19);
+            groupBox3.Controls.Add(label18);
+            groupBox3.Controls.Add(button3);
+            groupBox3.Controls.Add(textBox3);
+            groupBox3.Controls.Add(label17);
+            groupBox3.Controls.Add(label16);
+            groupBox3.Location = new Point(586, 375);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(378, 260);
+            groupBox3.TabIndex = 6;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "ทดสอบReference Parameter";
+            // 
+            // label20
+            // 
+            label20.AutoSize = true;
+            label20.Location = new Point(124, 161);
+            label20.Name = "label20";
+            label20.Size = new Size(58, 20);
+            label20.TabIndex = 6;
+            label20.Text = "label20";
+            // 
+            // label19
+            // 
+            label19.AutoSize = true;
+            label19.Location = new Point(124, 123);
+            label19.Name = "label19";
+            label19.Size = new Size(58, 20);
+            label19.TabIndex = 5;
+            label19.Text = "label19";
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Location = new Point(34, 161);
+            label18.Name = "label18";
+            label18.Size = new Size(48, 20);
+            label18.TabIndex = 4;
+            label18.Text = "ค่าใหม่";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(257, 67);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 52);
+            button3.TabIndex = 3;
+            button3.Text = "เพิ่มค่า";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(126, 67);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Location = new Point(34, 118);
+            label17.Name = "label17";
+            label17.Size = new Size(46, 20);
+            label17.TabIndex = 1;
+            label17.Text = "ค่าเดิม";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(34, 62);
+            label16.Name = "label16";
+            label16.Size = new Size(82, 20);
+            label16.TabIndex = 0;
+            label16.Text = "ตัวเลขเริ่มต้น";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(12, 139);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(175, 188);
+            textBox5.TabIndex = 7;
+            // 
+            // label21
+            // 
+            label21.AutoSize = true;
+            label21.Location = new Point(15, 106);
+            label21.Name = "label21";
+            label21.Size = new Size(113, 20);
+            label21.TabIndex = 8;
+            label21.Text = "กรุณากรอกตัวเลข";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(134, 104);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 9;
+            button4.Text = "ผลรวม";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1048, 711);
+            Controls.Add(button4);
+            Controls.Add(label21);
+            Controls.Add(textBox5);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form2";
+            Text = "Form2";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private GroupBox groupBox1;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Button button1;
+        private Label label7;
+        private GroupBox groupBox2;
+        private Label label15;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Button button2;
+        private Label label13;
+        private TextBox textBox4;
+        private Label label14;
+        private GroupBox groupBox3;
+        private TextBox textBox3;
+        private Label label17;
+        private Label label16;
+        private Label label20;
+        private Label label19;
+        private Label label18;
+        private Button button3;
+        private TextBox textBox5;
+        private Label label21;
+        private Button button4;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Winform.userDefineFunction
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double height = 0.0;
+            double weight = 0.0;
+            double triangleArea = 0.0;
+
+            if (!double.TryParse(textBox1.Text, out height)
+                && double.TryParse(textBox2.Text, out weight))
+            {
+                triangleArea = calculateTriangleArea(height, weight);
+                label6.Text = triangleArea.ToString("F2");
+
+            }
+        }
+
+        private double calculateTriangleArea(double height, double weight)
+        {
+            double Area = 0.5 * height * weight;
+            return Area;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double radius = 0.0;
+            double circleArea = 0.0;
+            double circumference = 0.0;
+
+            if (double.TryParse(textBox4.Text, out radius))
+            {
+                circleArea = calculateCircleArea(radius);
+                circumference = calculateCircumference(radius);
+                label15.Text = circumference.ToString("F2");
+                label9.Text = circleArea.ToString("F2");
+            }
+        }
+        private double calculateCircumference(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+        private double calculateCircleArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            if (int.TryParse(textBox3.Text, out i))
+            {
+                label19.Text = i.ToString();
+                label20.Text = RandomNextNumber(ref i).ToString();
+                textBox3.Text = i.ToString();
+
+            }
+        }
+        private int RandomNextNumber(ref int x)
+        {
+            Random rand = new Random();
+            x = rand.Next(1, 100);
+            return x;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double sumary = 0.0;
+            string[] inputs = textBox5.Lines;
+            double[] numbers = new double[inputs.Length];
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                if (!double.TryParse(inputs[i], out  numbers[i]))
+                {
+                    numbers[i] = 0.0;
+                }
+            }
+            sumary = CalculateSumary(numbers);
+            MessageBox.Show("ผลรวม = " + sumary.ToString("F2"));
+
+        }
+        private double CalculateSumary(double[] numbers)
+        {
+          
+            return numbers.Sum();
+        }
+    }
+}
